f27de79019da669186b512ff121d5480304d5eed
kfeutz <kevinfeutz@gmail.com>
2015-02-18 11:55:52 -0800

    Fixed route error causing bad gateway

--- routes/top10.js
+++ routes/top10.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  	res.render('top10',  { title: 'Express' }));
+  	res.render('top10',  { title: 'Express' });
 });
 
 module.exports = router;
